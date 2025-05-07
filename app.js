b48d90d347d03845bb8809bbde66d9e87d4d9722
kshitijcx <kshitij.cx@gmail.com>
2025-05-07 23:48:39 +0530

    integrate game logic with socket.io

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
+import MendicotGame from "./gameLogic.js";
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
+const game = new MendicotGame();
+
+io.on("connection", (socket) => {
+  console.log("Player connected:", socket.id);
+
+  const success = game.addPlayer(socket);
+  if (!success) {
+    socket.emit("roomFull");
+    socket.disconnect();
+    return;
+  }
+
+  socket.on("playCard", (card) => {
+    game.playCard(socket.id, card);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Player disconnected:", socket.id);
+    game.removePlayer(socket.id);
+  });
+});
+
 server.listen(3000, () => {
-  console.log("Mendicot server running on port 3000");
+  console.log("Mendicot server running at http://localhost:3000");
 });
